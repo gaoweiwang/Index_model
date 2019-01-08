a0774cb2ac6940576f3f7859cd80436500d500a0
wanyewang1 <35814792+wanyewang1@users.noreply.github.com>
2019-01-07 20:53:25 -0800

    Add files via upload

---
+++ model_testing.R
@@ -0,0 +1,129 @@
+
+#Input: training data, testing data
+x=read.csv('/mnt/dell1/gaowei/model/data/rep_new_norm_20.csv')
+x=x[,c(1,14:16,26:28,32:34,38:40,44:46,59:61,65:67,71:73,89:91,56:58,86:88,96:100)]
+y=c(rep(1,27),rep(2,11))
+#z=read.csv('/mnt/dell1/gaowei/model/data/rep_new_norm_20.csv')
+z=read.csv('/mnt/dell1/gaowei/model/data/Human_data1/GSE89632_NASH.csv')
+#z=read.csv('/mnt/dell1/gaowei/model/data/Human_data/GSE14520_GPL3921_ID.csv')
+#z=read.csv('/mnt/dell1/gaowei/model/data/Human_data/GSE16757.csv')
+
+###data normalization
+x$X=toupper(as.character(x$X))
+z=z[!is.na(z$X),]
+z$X=toupper(as.character(z$X))
+
+Data_all=merge(x,z,by='X',ALL=FALSE)
+x_train=Data_all[,c(1:dim(x)[2])]
+t_s=(dim(x)[2]+1)
+t_e=dim(Data_all)[2]
+z_test=Data_all[,c(1,t_s:t_e)]
+
+x_train_m=as.matrix(x_train[,-1])
+rownames(x_train_m)=toupper(as.character(x_train$X))
+z_test_m=as.matrix(z_test[,-1])
+rownames(z_test_m)=toupper(as.character(z_test$X))
+
+x_ref=normalize.quantiles(x_train_m)
+rownames(x_ref)=rownames(x_train_m)
+colnames(x_ref)=colnames(x_train_m)
+
+temp_sort=sort(x_ref[,1])
+for (i in 1:dim(x_train_m)[2]){
+  x_train_m[,i]=temp_sort[rank(x_train_m[,i])]
+}
+for (i in 1:dim(z_test_m)[2]){
+  z_test_m[,i]=temp_sort[rank(z_test_m[,i])]
+}
+
+### deviation of TFs
+GSA_result=Readout[[1]]
+D_normal=matrix(1000,dim(z_test_m)[2],dim(GSA_result)[1])
+rownames(D_normal)=colnames(z_test_m)
+colnames(D_normal)=rownames(GSA_result)
+D_cancer=matrix(1000,dim(z_test_m)[2],dim(GSA_result)[1])
+rownames(D_cancer)=colnames(z_test_m)
+colnames(D_cancer)=rownames(GSA_result)
+
+#deviatino of gene expression pattern
+Ref_normal=x_train_m[,y==1]
+Ref_cancer=x_train_m[,y==2]
+M_weight=Readout[[2]]
+M_weight1=matrix(0,dim(z_test_m),dim(M_weight)[2])
+rownames(M_weight1)=rownames(z_test_m)
+colnames(M_weight1)=colnames(M_weight)
+M1_genelist=rownames(M_weight1)
+M_genelist=rownames(M_weight)
+for (M1_i in 1:dim(M_weight1)[1]){
+  for (M_i in 1:dim(M_weight)[1]){
+    if (M1_genelist[M1_i]==M_genelist[M_i]){
+      M_weight1[M1_i,]=M_weight[M_i,]
+      break
+    }
+  }
+}
+for (temp_i in 1:dim(D_normal)[1]){
+  temp_pro = z_test_m[,temp_i]
+  normal_zscore = rep(0,length(temp_pro))
+  cancer_zscore = rep(0,length(temp_pro))
+  for (temp_genes in 1:length(temp_pro)){
+    #normal
+    temp_ave=mean(Ref_normal[temp_genes,])
+    temp_sd=sd(Ref_normal[temp_genes,])
+    if (temp_ave>0 & temp_sd>0){
+      normal_zscore[temp_genes]=abs((temp_pro[temp_genes]-temp_ave)/temp_sd)
+    }
+    #cancer 
+    temp_ave=mean(Ref_cancer[temp_genes,])
+    temp_sd=sd(Ref_cancer[temp_genes,])
+    if (temp_ave>0 & temp_sd>0){
+      cancer_zscore[temp_genes]=abs((temp_pro[temp_genes]-temp_ave)/temp_sd)
+    }
+  }
+  
+  for (temp_j in 1:dim(D_normal)[2]){
+    D_normal[temp_i,temp_j] = normal_zscore %*% M_weight1[,temp_j]
+    D_cancer[temp_i,temp_j] = cancer_zscore %*% M_weight1[,temp_j]
+  }
+}
+
+### adjusted activity of TF clusters (normal liver: -1, HCC:1)
+A_TFactivity=matrix(0,dim(z_test_m)[2],dim(GSA_result)[1])
+rownames(A_TFactivity)=colnames(z_test_m)
+colnames(A_TFactivity)=rownames(GSA_result)
+sign_TF=rep(0,dim(A_TFactivity)[2])
+sign_TF[(GSA_result[,1]>0)]=1
+sign_TF[(GSA_result[,1]<0)]=(-1)
+
+for (ti in 1:dim(A_TFactivity)[1]){
+  for (tj in 1:dim(A_TFactivity)[2]){
+    A_TFactivity[ti,tj]=((D_normal[ti,tj]-D_cancer[ti,tj])/(D_normal[ti,tj]+D_cancer[ti,tj]))*sign_TF[tj]
+  }
+}
+
+## average activity of promoting and suppressing TF group
+TFcluster=matrix(0,dim(A_TFactivity)[1],2)
+colnames(TFcluster)=c('T_pro','T_supp')
+rownames(TFcluster)=rownames(A_TFactivity)
+temp_normalTF=A_TFactivity[,(sign_TF==(-1))]
+temp_cancerTF=A_TFactivity[,(sign_TF==(1))]
+TFcluster[,1]=rowMeans(temp_cancerTF)
+TFcluster[,2]=rowMeans(temp_normalTF)
+
+## index
+fit=Readout[[6]]
+Sample_index=predict(fit,newx=TFcluster,s=c(0.1,0.05))
+
+write.csv(Sample_index,'GSE89632_index.csv')
+all_varlist=ls()
+rm_var=(all_varlist[!(all_varlist=='Readout')])
+rm(list=rm_var)
+
+A=read.csv('/other/R_0618/GSE66676_index_no.csv')
+tt=as.numeric(A$X1)
+tt1=tt[1:34]
+tt2=tt[35:60]
+
+mean(tt1)
+mean(tt2)
+t.test(tt2,tt1)
---
+++ model_training.R
@@ -0,0 +1,172 @@
+#library
+rm(list=ls())
+
+library(GSA)
+library(randomForest)
+library(preprocessCore)
+library(glmnet)
+
+## input
+x=read.csv('/mnt/dell1/gaowei/model/data/rep_new_norm_20.csv')
+x=x[,c(1,14:16,26:28,32:34,38:40,44:46,59:61,65:67,71:73,89:91,56:58,86:88,96:100)]
+y=c(rep(1,27),rep(2,11))
+z=read.csv('/mnt/dell1/gaowei/model/data/TFcluster_positive_update.csv')
+
+## significantly changed TF clusters
+x_m=as.matrix(x[,-1])
+rownames(x_m)=toupper(as.character(x$X))
+x1=x_m
+y1=y
+
+gene_sets=list(1)
+for (i in 1:dim(z)[2]){
+  gene_sets[[i]]=toupper(unique(as.character(z[,i])))
+}
+genenames= toupper(rownames(x1))
+GSA_temp=GSA(x1,y1, genesets=gene_sets, genenames,method="maxmean", resp.type="Two class unpaired",minsize=40,maxsize=5000,nperms=1000)
+GSA_result=cbind(GSA_temp$GSA.scores,GSA_temp$pvalues.lo,GSA_temp$pvalues.hi)
+colnames(GSA_result)=c('score','pvalue_low','pvalue_high')
+rownames(GSA_result)=toupper(colnames(z))
+GSA_result1=GSA_result[!is.na(GSA_temp$GSA.scores),]
+keep_set=(abs(GSA_result1[,1])>0.8 & (GSA_result1[,2]<0.05 | GSA_result1[,3]<0.05))
+GSA_result2=GSA_result1[keep_set,]
+
+### heatmap
+# code in TF.R
+
+### weight matrix (genes*TF) using randomforest
+#data normalization
+x_f=normalize.quantiles(x_m)
+rownames(x_f)=rownames(x_m)
+colnames(x_f)=colnames(x_m)
+
+#weight matrix
+M=matrix(0,dim(GSA_result2)[1],dim(x_f)[1])
+rownames(M)=rownames(GSA_result2)
+colnames(M)=rownames(x_f)
+
+for (i in 1:dim(M)[1]){
+  temp_TF=rownames(M)[i]
+  temp_genes=unique(toupper(as.character(z[,temp_TF==colnames(z)])))
+  temp_keep=(rownames(x_f)=='plplpl')
+  for (j in 1:length(temp_genes)){
+    temp_keep1=(temp_genes[j]==toupper(rownames(x_f)))
+    temp_keep=(temp_keep | temp_keep1)
+  }
+  x_temp=x_f[temp_keep,]
+  x_temp1=rbind(y,x_temp)
+  x_forest=as.data.frame(t(x_temp1))
+  
+  sample.ind <- sample(2, nrow(x_forest),replace = T,prob = c(1,0))
+  x_dev <- x_forest[sample.ind==1,]
+  x_val <- x_forest[sample.ind==2,]
+  
+  varNames <- names(x_dev)
+  varNames <- varNames[!varNames %in% c("y")]
+  temp_var=varNames
+  varNames1 <- paste('V',1:length(varNames), sep = "")
+  colnames(x_dev)=c('y',varNames1)
+  varNames2 <- paste(varNames1, collapse = "+")
+  rf.form <- as.formula(paste("y", varNames2, sep = " ~ "))
+  x_dev$y=as.factor(x_dev$y)
+  cross.sell.rf <- randomForest(rf.form,x_dev,ntree=1000,importance=T)
+  TF_M=cross.sell.rf$importance
+  rownames(TF_M)=temp_var
+  
+  temp_listi=rownames(TF_M)
+  temp_listj=colnames(M)
+  for (temp_i in 1:dim(TF_M)[1]){
+    for (temp_j in 1:dim(M)[2]){
+      if (temp_listi[temp_i]==temp_listj[temp_j]){
+        M[i,temp_j]=TF_M[temp_i,4]
+      }
+    }
+  }
+}
+M_weight=t(M)
+
+###deviation of samples: samples*TFs (normal liver and HCC)
+D_normal=matrix(1000,dim(x_m)[2],dim(GSA_result2)[1])
+rownames(D_normal)=colnames(x_m)
+colnames(D_normal)=rownames(GSA_result2)
+D_cancer=matrix(1000,dim(x_m)[2],dim(GSA_result2)[1])
+rownames(D_cancer)=colnames(x_m)
+colnames(D_cancer)=rownames(GSA_result2)
+
+#deviatino of gene expression pattern
+Ref_normal=x_m[,y==1]
+Ref_cancer=x_m[,y==2]
+
+for (temp_i in 1:dim(D_normal)[1]){
+  temp_pro = x_m[,temp_i]
+  normal_zscore = rep(0,length(temp_pro))
+  cancer_zscore = rep(0,length(temp_pro))
+  for (temp_genes in 1:length(temp_pro)){
+    #normal
+    temp_ave=mean(Ref_normal[temp_genes,])
+    temp_sd=sd(Ref_normal[temp_genes,])
+    if (temp_ave>0 & temp_sd>0){
+      normal_zscore[temp_genes]=abs((temp_pro[temp_genes]-temp_ave)/temp_sd)
+    }
+      #cancer 
+      temp_ave=mean(Ref_cancer[temp_genes,])
+      temp_sd=sd(Ref_cancer[temp_genes,])
+      if (temp_ave>0 & temp_sd>0){
+        cancer_zscore[temp_genes]=abs((temp_pro[temp_genes]-temp_ave)/temp_sd)
+      }
+    }
+  
+  for (temp_j in 1:dim(D_normal)[2]){
+    D_normal[temp_i,temp_j] = normal_zscore %*% M_weight[,temp_j]
+    D_cancer[temp_i,temp_j] = cancer_zscore %*% M_weight[,temp_j]
+  }
+}
+
+### adjusted activity of TF clusters (normal liver: -1, HCC:1)
+A_TFactivity=matrix(0,dim(x_m)[2],dim(GSA_result2)[1])
+rownames(A_TFactivity)=colnames(x_m)
+colnames(A_TFactivity)=rownames(GSA_result2)
+sign_TF=rep(0,dim(A_TFactivity)[2])
+sign_TF[(GSA_result2[,1]>0)]=1
+sign_TF[(GSA_result2[,1]<0)]=(-1)
+
+for (ti in 1:dim(A_TFactivity)[1]){
+  for (tj in 1:dim(A_TFactivity)[2]){
+    A_TFactivity[ti,tj]=((D_normal[ti,tj]-D_cancer[ti,tj])/(D_normal[ti,tj]+D_cancer[ti,tj]))*sign_TF[tj]
+  }
+}
+
+## average activity of promoting and suppressing TF group
+TFcluster=matrix(0,dim(A_TFactivity)[1],2)
+colnames(TFcluster)=c('T_pro','T_supp')
+rownames(TFcluster)=rownames(A_TFactivity)
+temp_normalTF=A_TFactivity[,(sign_TF==(-1))]
+temp_cancerTF=A_TFactivity[,(sign_TF==(1))]
+TFcluster[,1]=rowMeans(temp_cancerTF)
+TFcluster[,2]=rowMeans(temp_normalTF)
+
+## weights of TF clusters in determining phenotypes
+res_vector=y
+res_vector[y==1]=(-1)
+res_vector[y==2]=1
+var_input=TFcluster
+fit = glmnet(var_input, res_vector)
+weig=coef(fit,s=0.1)
+
+
+## index
+Sample_index=predict(fit,newx=TFcluster,s=c(0.1,0.05))
+
+#####
+Readout=list(1)
+Readout[[1]]=GSA_result2
+Readout[[2]]=M_weight
+Readout[[3]]=A_TFactivity
+Readout[[4]]=TFcluster
+Readout[[5]]=Sample_index
+Readout[[6]]=fit
+
+all_varlist=ls()
+rm_var=(all_varlist[!(all_varlist=='Readout')])
+rm(list=rm_var)
+
